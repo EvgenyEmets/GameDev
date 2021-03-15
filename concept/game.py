ccb5ea48052239a77db52bef59743efce66fc925
EvgenyEmets <e-evgeny99@ya.ru>
2021-03-15 23:46:50 +0300

    little refactoring

--- concept/game.py
+++ concept/game.py
@@ -11,20 +11,19 @@ info = pygame.display.Info()
 size = width, height = info.current_w, info.current_h
 print(size)
 
-game_map = [[[0] for i in range(5)] for j in range(5)]
-for i in range(5):
-    for j in range(5):
-        game_map[i][j] = random.randint(0, 5)
+map_size = 50
+
+game_map = [[random.randint(0, 5) for i in range(map_size)] for j in range(map_size)]
 print(game_map)
+drow_map = [[pygame.Rect(i*50, j*50, 50, 50) for i in range(map_size)] for j in range(map_size)]
+for i in range(map_size):
+    for j in range(map_size):
+        color_val = game_map[i][j] * 50
+        color = (color_val, color_val, color_val)
+        pygame.draw.rect(screen, color, drow_map[i][j])
+pygame.display.flip()
 
 pos_x = 0;
 pos_y = 0;
 
-drow_map = [[pygame.Rect(i*50, j*50, 50, 50) for i in range(5)] for j in range(5)]
-for i in range(5):
-    for j in range(5):
-        color_val = game_map[i][j] * 50
-        color = (color_val, color_val, color_val)
-        pygame.draw.rect(screen, color, drow_map[i][j])
-        pygame.display.flip()
-time.sleep(1)
+time.sleep(5)
